088a372ad9db826668ce041bc197a271aa75390b
robertopc <robertopc.prog@gmail.com>
2022-07-19 19:47:36 -0300

    Added auth controller and route to login

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->get_tokens;
 });
+
+//API route for register new user
+Route::post('/register', [AuthController::class, 'register']);
+//API route for login user
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+Route::get('/login', ['as' => 'login', function(){
+    return response()->json(['message' => 'Unauthorized.'], 401);
+}]);
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+
+        return auth()->user();
+    });
+
+    // API route for logout user
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+});
+
